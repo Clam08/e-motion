4fd87e7b2dcd4a195811f035642f86fa0ac0e71c
milo shields <49406817+miloshields@users.noreply.github.com>
2020-12-05 12:28:00 -0500

    pages for basic entry and analysis functionality
    no database access yet

---
+++ entry.js
@@ -0,0 +1,229 @@
+
+var emotion_score;
+/* not my code, used to replace chars with %20 that are not request-appropriate*/
+function URLify(string) {
+  return string.trim().replace(/\s/g, '%20');
+}
+
+
+/* to be called when request is successfully received in order to visualize
+emotional content */
+function showScore(){
+    console.log("Starting to show score.")
+    //document.getElementsByClassName("wrap")[0].innerHTML = emotion_score;
+    emotion_score *=100;
+    emotion_score /= 2;
+    emotion_score += 50;
+    var emotion_string = Math.round(emotion_score).toString()
+    var containers = document.getElementsByClassName("chart");
+    document.getElementsByClassName("dial")[0].style.display = "flex";
+    document.getElementsByClassName("wrap")[0].style.display = "none";
+    containers[0].setAttribute("data-value", emotion_string);
+    var dial = new Dial(containers[0]);
+    dial.animateStart();
+}
+
+/* to be called when the user needs to analyze their data */
+function getResponse(){
+    /* get data delimited by %20s from text elem*/
+    var contenteditable = document.querySelector('[contenteditable]'),
+    text = contenteditable.textContent;
+    text = URLify(text);
+    console.log(text)
+    const data = "text="+text;
+    console.log(data);
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+    	if (this.readyState === this.DONE) {
+    		console.log(this.responseText);
+            analysis_obj = JSON.parse(this.responseText);
+            emotion_score = analysis_obj["score"];
+            showScore();
+    	}
+    });
+
+    xhr.open("POST", "https://twinword-sentiment-analysis.p.rapidapi.com/analyze/");
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("x-rapidapi-key", "34a6905a16msh38f44642663039ap15cd20jsn3ef0bc3e0487");
+    xhr.setRequestHeader("x-rapidapi-host", "twinword-sentiment-analysis.p.rapidapi.com");
+
+    xhr.send(data);
+}
+document.getElementById("analyze").addEventListener("click",getResponse);
+
+var Dial = function(container) {
+    this.container = container;
+    this.size = this.container.dataset.size;
+    this.strokeWidth = this.size / 8;
+    this.radius = (this.size / 2) - (this.strokeWidth / 2);
+    this.value = this.container.dataset.value;
+    this.direction = this.container.dataset.arrow;
+    this.svg;
+    this.defs;
+    this.slice;
+    this.overlay;
+    this.text;
+    this.arrow;
+    this.create();
+}
+
+Dial.prototype.create = function() {
+    this.createSvg();
+    this.createDefs();
+    this.createSlice();
+    this.createOverlay();
+    this.createText();
+    this.createArrow();
+    this.container.appendChild(this.svg);
+};
+
+Dial.prototype.createSvg = function() {
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute('width', this.size + 'px');
+    svg.setAttribute('height', this.size + 'px');
+    this.svg = svg;
+};
+
+Dial.prototype.createDefs = function() {
+    var defs = document.createElementNS("http://www.w3.org/2000/svg", "defs");
+    var linearGradient = document.createElementNS("http://www.w3.org/2000/svg", "linearGradient");
+    linearGradient.setAttribute('id', 'gradient');
+    var stop1 = document.createElementNS("http://www.w3.org/2000/svg", "stop");
+    stop1.setAttribute('stop-color', '#6E4AE2');
+    stop1.setAttribute('offset', '0%');
+    linearGradient.appendChild(stop1);
+    var stop2 = document.createElementNS("http://www.w3.org/2000/svg", "stop");
+    stop2.setAttribute('stop-color', '#78F8EC');
+    stop2.setAttribute('offset', '100%');
+    linearGradient.appendChild(stop2);
+    var linearGradientBackground = document.createElementNS("http://www.w3.org/2000/svg", "linearGradient");
+    linearGradientBackground.setAttribute('id', 'gradient-background');
+    var stop1 = document.createElementNS("http://www.w3.org/2000/svg", "stop");
+    stop1.setAttribute('stop-color', 'rgba(0, 0, 0, 0.2)');
+    stop1.setAttribute('offset', '0%');
+    linearGradientBackground.appendChild(stop1);
+    var stop2 = document.createElementNS("http://www.w3.org/2000/svg", "stop");
+    stop2.setAttribute('stop-color', 'rgba(0, 0, 0, 0.05)');
+    stop2.setAttribute('offset', '100%');
+    linearGradientBackground.appendChild(stop2);
+    defs.appendChild(linearGradient);
+    defs.appendChild(linearGradientBackground);
+    this.svg.appendChild(defs);
+    this.defs = defs;
+};
+
+Dial.prototype.createSlice = function() {
+    var slice = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    slice.setAttribute('fill', 'none');
+    slice.setAttribute('stroke', 'url(#gradient)');
+    slice.setAttribute('stroke-width', this.strokeWidth);
+    slice.setAttribute('transform', 'translate(' + this.strokeWidth / 2 + ',' + this.strokeWidth / 2 + ')');
+    slice.setAttribute('class', 'animate-draw');
+    this.svg.appendChild(slice);
+    this.slice = slice;
+};
+
+Dial.prototype.createOverlay = function() {
+    var r = this.size - (this.size / 2) - this.strokeWidth / 2;
+    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute('cx', this.size / 2);
+    circle.setAttribute('cy', this.size / 2);
+    circle.setAttribute('r', r);
+    circle.setAttribute('fill', 'url(#gradient-background)');
+    this.svg.appendChild(circle);
+    this.overlay = circle;
+};
+
+Dial.prototype.createText = function() {
+    var fontSize = this.size / 3.5;
+    var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    text.setAttribute('x', (this.size / 2) + fontSize / 7.5);
+    text.setAttribute('y', (this.size / 2) + fontSize / 4);
+    text.setAttribute('font-family', 'Century Gothic, Lato');
+    text.setAttribute('font-size', fontSize);
+    text.setAttribute('fill', '#78F8EC');
+    text.setAttribute('text-anchor', 'middle');
+    var tspanSize = fontSize / 3;
+    text.innerHTML = 0 + '<tspan font-size="' + tspanSize + '" dy="' + -tspanSize * 1.2 + '">%</tspan>';
+    this.svg.appendChild(text);
+    this.text = text;
+};
+
+Dial.prototype.createArrow = function() {
+    var arrowSize = this.size / 10;
+    var arrowYOffset, m;
+    if(this.direction === 'up') {
+        arrowYOffset = arrowSize / 2;
+        m = -1;
+    }
+    else if(this.direction === 'down') {
+        arrowYOffset = 0;
+        m = 1;
+    }
+    var arrowPosX = ((this.size / 2) - arrowSize / 2);
+    var arrowPosY = (this.size - this.size / 3) + arrowYOffset;
+    var arrowDOffset =  m * (arrowSize / 1.5);
+    var arrow = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    arrow.setAttribute('d', 'M 0 0 ' + arrowSize + ' 0 ' + arrowSize / 2 + ' ' + arrowDOffset + ' 0 0 Z');
+    arrow.setAttribute('fill', '#97F8F0');
+    arrow.setAttribute('opacity', '0.6');
+    arrow.setAttribute('transform', 'translate(' + arrowPosX + ',' + arrowPosY + ')');
+    this.svg.appendChild(arrow);
+    this.arrow = arrow;
+};
+
+Dial.prototype.animateStart = function() {
+    var v = 0;
+    var self = this;
+    var intervalOne = setInterval(function() {
+        var p = +(v / self.value).toFixed(2);
+        var a = (p < 0.95) ? 2 - (2 * p) : 0.05;
+        v += a;
+        // Stop
+        if(v >= +self.value) {
+            v = self.value;
+            clearInterval(intervalOne);
+        }
+        self.setValue(v);
+    }, 10);
+};
+
+Dial.prototype.animateReset = function() {
+    this.setValue(0);
+};
+
+Dial.prototype.polarToCartesian = function(centerX, centerY, radius, angleInDegrees) {
+  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+  return {
+    x: centerX + (radius * Math.cos(angleInRadians)),
+    y: centerY + (radius * Math.sin(angleInRadians))
+  };
+}
+
+Dial.prototype.describeArc = function(x, y, radius, startAngle, endAngle){
+    var start = this.polarToCartesian(x, y, radius, endAngle);
+    var end = this.polarToCartesian(x, y, radius, startAngle);
+    var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+    var d = [
+        "M", start.x, start.y,
+        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+    ].join(" ");
+    return d;
+}
+
+Dial.prototype.setValue = function(value) {
+		var c = (value / 100) * 360;
+		if(c === 360)
+			c = 359.99;
+		var xy = this.size / 2 - this.strokeWidth / 2;
+		var d = this.describeArc(xy, xy, xy, 180, 180 + c);
+    this.slice.setAttribute('d', d);
+    var tspanSize = (this.size / 3.5) / 3;
+    this.text.innerHTML = Math.floor(value) + '<tspan font-size="' + tspanSize + '" dy="' + -tspanSize * 1.2 + '">%</tspan>';
+};
+
+//
+// Usage
+//

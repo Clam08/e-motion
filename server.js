fc89a72130deefb927cf955a19f7cb08e31b3158
Muyin-Yao <yaomuyin@gmail.com>
2020-12-11 15:54:18 -0500

    communication between server and browser set up for login and journal entries set up

--- server.js
+++ server.js
@@ -8,24 +8,25 @@ var port = process.env.PORT || 3000;
 
 const users = [];
 
-async function verify_user_password(user_n, password) {
+async function verify_user_password(user_email, password) {
   const client = await MongoClient.connect(mongo_url)
   try {
     const dbo = client.db("emotion");
     var coll = dbo.collection('users');
     
-    theQuery = {user:user_n}
-    var items_found = await coll.find(theQuery).toArray();
+    theQuery = {email:user_email}
     
+    var items_found = await coll.find(theQuery).toArray();
     for (i=0; i<items_found.length; i++){
       if(password == items_found[i].password){
         console.log("match")
         user = {
           id: items_found[i]._id,
+          email: items_found[i].email,
           name: items_found[i].user
         }
         users.push(user);
-        return user;
+        return "match";
       }
     }
   } catch (err) {
@@ -35,14 +36,14 @@ async function verify_user_password(user_n, password) {
   }
 }
 
-async function insert_users(user_n, password) {
+async function insert_users(user_n, user_email, password) {
     await MongoClient.connect(mongo_url, function(err, db) {
     if (err) throw err;
   
     const dbo = db.db("emotion");
     var collection = dbo.collection('users');
     
-    var newData = {"user": user_n, "password": password};
+    var newData = {"user": user_n, "email": user_email, "password": password};
     collection.insertOne(newData, function(err, res) {
       if (err) throw err;
       console.log("new document inserted");
@@ -58,28 +59,25 @@ app.get('/process_get', async (req, res) => { //input user, take to login page
   var qobj = url.parse(req.url, true).query; //parse the query
   
   user_n = qobj.user_name;
+  user_email = qobj.email;
   password = qobj.password; 
-  await insert_users(user_n, password);
+  await insert_users(user_n, user_email, password);
   
   res.redirect("/login.html" );
   res.end();
 })
 
-app.get("/random", (req, res) => {
-  res.status(200).json({message: "what is the matter"});
-})
-
 app.get('/process_login', async (req, res) => { //check users, take to main page
   var qobj = url.parse(req.url, true).query; //parse the query
-  user_n = qobj.user_name;
+  user_email = qobj.email;
   password = qobj.password; 
   
-  result = await verify_user_password(user_n, password);
+  result = await verify_user_password(user_email, password);
   console.log("result " + result)
   
-  if (result != ""){
-    res.redirect('/index.html')
-  }  else {
+  if (result == "match"){
+    res.redirect( "index.html" );
+  } else {
     res.redirect( "register.html" );
   }
   res.end();
@@ -87,8 +85,53 @@ app.get('/process_login', async (req, res) => { //check users, take to main page
 
 app.get('/user', (req, res) => res.send(users[0]));
 
-app.post('/blog', (req, res) => {
-  res.send({message:"helldfadfadfdo"})
+app.get('/blog', async (req, res) => {
+  user_id = req.headers.email;
+  console.log("user_id " + user_id);
+  
+  const client = await MongoClient.connect(mongo_url)
+  try {
+    const dbo = client.db("emotion");
+    var coll = dbo.collection('users');
+    
+    theQuery = {email:user_id}
+    
+    var items_found = await coll.find(theQuery).toArray();
+    
+    res.send(items_found[0].entries);
+  } catch (err) {
+      console.log(err);
+  } finally {
+      client.close();
+  }
+});
+
+app.get('/entry_get', async (req, res) => {
+  var qobj = url.parse(req.url, true).query; //parse the query
+  
+  user_text = qobj.content;
+  console.log("user_text " + user_text);
+  
+  const client = await MongoClient.connect(mongo_url)
+  try {
+    const dbo = client.db("emotion");
+    var coll = dbo.collection('users');
+    
+    user_id = users[0].email;
+    theQuery = {email:user_id}
+    updateDocument = {
+      $push: { "entries": user_text },
+    };
+    
+    await coll.updateOne(theQuery, updateDocument);
+    console.log("push success");
+    res.redirect("index.html");
+    
+  } catch (err) {
+      console.log(err);
+  } finally {
+      client.close();
+  }
 });
 
 var server = app.listen(port, function () {
